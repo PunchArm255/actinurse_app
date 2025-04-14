90bafc602ed5bd79edbd1769848f7439579ea998
PunchArm255 <simohamednassiri@gmail.com>
2025-04-14 04:46:25 +0100

    structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Stats from "./pages/Stats";
+import Welcome from "./pages/welcome";
+import PostPreview from "./pages/PostPreview";
+import { UserProvider } from "./lib/context/user";
+import { ThemeProvider } from "./lib/context/theme";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/journal" element={<Journal />} />
+            <Route path="/reminder" element={<Reminder />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/progress" element={<Progress />} />
+            <Route path="/stock" element={<Stock />} />
+          </Routes>
+        </Router>
+      </UserProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;

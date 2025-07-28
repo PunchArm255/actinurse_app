167701d06cde9329e9ca7bd662f93adf84d81609
PunchArm255 <simohamednassiri@gmail.com>
2025-07-28 13:01:34 +0100

    hero update

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -181,7 +181,7 @@ const Welcome = () => {
                             whileHover={{ scale: 1.02 }}
                             transition={{ type: "spring", stiffness: 300 }}
                         >
-                            Votre Assistant
+                            Ton Allié Numérique
                             <motion.span
                                 className="block bg-gradient-to-r from-yellow-300 to-pink-300 bg-clip-text text-transparent"
                                 animate={{
@@ -193,7 +193,7 @@ const Welcome = () => {
                                     ease: "linear"
                                 }}
                             >
-                                Médical Intelligent
+                                pour Mieux Soigner
                             </motion.span>
                         </motion.h2>
 
